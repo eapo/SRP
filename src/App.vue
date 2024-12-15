5b1a17bbc17187b69dd3596b81c3a5f7267828fc
eapo <e@eaposztrof.com>
2024-12-15 09:04:17 +0200

    create landing

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import index from './components/index.vue'
 </script>
 
 <template>
@@ -7,11 +7,8 @@ import HelloWorld from './components/HelloWorld.vue'
     <a href="https://vite.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <index msg="Shared Resources Plugin" />
 </template>
 
 <style scoped>
@@ -23,8 +20,5 @@ import HelloWorld from './components/HelloWorld.vue'
 }
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
---
+++ src/components/index.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <ol>
+		<li>
+		  <code>&lt;head></code>: <code>&lt;script
+  src="https://cdn.../srp.min.js" crossorigin="anonymous" integrity="sha256-/...">&lt;/script></code>
+		</li><li>
+		  <code>&lt;body></code>: <code>&lt;fieldset id="srp">&lt;legend>Shared Resources&lt;/legend></code></li>
+    </ol>
+  </div>
+
+	<p><span>A </span><em><span>GPT</span></em><span> guided </span><em><span>Secure Plugin</span></em><span> to connect </span><em><span>Resources</span></em><span> and </span><em><span>Needs</span></em><span> using Additional Incentives. </span><code>#CoNetRes</code></p>
+
+  <p class="read-the-docs"></p>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+ol {
+	text-align: left;
+	}
+</style>
